c34a143b8084f50c53da5e36076ccb7f94340711
Isaac Santiago <isaac.pereira@zumpy.com.br>
2024-02-21 15:19:30 -0300

    build: create spring boot project

---
+++ src/main/java/org/zumpy/zumpythundera/ZumpyThunderaApplication.java
@@ -0,0 +1,13 @@
+package org.zumpy.zumpythundera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZumpyThunderaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZumpyThunderaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zumpy/zumpythundera/ZumpyThunderaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zumpy.zumpythundera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZumpyThunderaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
